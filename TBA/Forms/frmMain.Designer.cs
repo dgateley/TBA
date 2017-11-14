2b80d65b1a2bf0c6cc01c38dbcd53e7c12a6bf72
dlgateley@cst.local <dlgateley@cst.local>
2017-11-14 13:09:13 -0600

    Initial commit

---
+++ TBA/Forms/frmMain.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TBA
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.itmFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.itmOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.itmExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.itmHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.itmAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itmFile,
+            this.itmHelp});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(1050, 24);
+            this.mnuMenu.TabIndex = 0;
+            this.mnuMenu.Text = "menuStrip1";
+            // 
+            // itmFile
+            // 
+            this.itmFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itmOpen,
+            this.itmExit});
+            this.itmFile.Name = "itmFile";
+            this.itmFile.Size = new System.Drawing.Size(37, 20);
+            this.itmFile.Text = "File";
+            // 
+            // itmOpen
+            // 
+            this.itmOpen.Name = "itmOpen";
+            this.itmOpen.Size = new System.Drawing.Size(152, 22);
+            this.itmOpen.Text = "Open";
+            // 
+            // itmExit
+            // 
+            this.itmExit.Name = "itmExit";
+            this.itmExit.Size = new System.Drawing.Size(152, 22);
+            this.itmExit.Text = "Exit";
+            // 
+            // itmHelp
+            // 
+            this.itmHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itmAbout});
+            this.itmHelp.Name = "itmHelp";
+            this.itmHelp.Size = new System.Drawing.Size(44, 20);
+            this.itmHelp.Text = "Help";
+            // 
+            // itmAbout
+            // 
+            this.itmAbout.Name = "itmAbout";
+            this.itmAbout.Size = new System.Drawing.Size(152, 22);
+            this.itmAbout.Text = "About";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 817);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
+            this.Name = "frmMain";
+            this.Text = "TBA";
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.ToolStripMenuItem itmFile;
+        private System.Windows.Forms.ToolStripMenuItem itmOpen;
+        private System.Windows.Forms.ToolStripMenuItem itmExit;
+        private System.Windows.Forms.ToolStripMenuItem itmHelp;
+        private System.Windows.Forms.ToolStripMenuItem itmAbout;
+    }
+}
+
